70b11d7080bbeb96bc03c9b2f5de16480f68a124
rajath99 <84463733+rajath99@users.noreply.github.com>
2026-05-13 13:55:45 +0530

    Update Process.jsx

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -10,7 +10,9 @@ export default function Process() {
     <section id="process" className="py-32">
       <div className="max-w-5xl mx-auto px-6">
         <div className="text-center mb-20">
-          <p className="text-yellow-400 uppercase tracking-[5px] mb-4">
+          <p <p className="uppercase tracking-[6px] text-yellow-500 text-lg font-bold mb-6">
+  Luxury Real Estate & Interiors
+</p>>
             Our Process
           </p>
 
